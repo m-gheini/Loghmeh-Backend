c7eef71282b27df5d5f7bd00bcbc56cc6b1de54f
leilafakheri <fakheri90@gmail.com>
2020-02-20 15:37:34 +0330

    first test servlet

--- src/main/java/IECA/servlets/getRestaurants.java
+++ src/main/java/IECA/servlets/getRestaurants.java
@@ -1,54 +1,21 @@
 package IECA.servlets;
 
-import IECA.database.DatasetManager;
-import IECA.logic.Restaurant;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+@WebServlet(name = "GetRestaurants")
+public class GetRestaurants extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-@WebServlet("/getRestaurants")
-public class getRestaurants extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws IOException {
-        DatasetManager dataset = new DatasetManager();
-        PrintWriter out = response.getWriter();
-        out.println("<!DOCTYPE html>");
-        out.println("<html lang=\"en\">");
-        out.println("<head>");
-        out.println("<meta charset=\"UTF-8\">");
-        out.println("<title>"+"getRestaurants"+"</title>");
-        out.println("        table {");
-        out.println("            text-align: center;");
-        out.println("            margin: auto;");
-        out.println("        }");
-        out.println("        th, td {");
-        out.println("            padding: 5px;");
-        out.println("            text-align: center;");
-        out.println("        }");
-        out.println( "        .logo{");
-        out.println("            width: 100px;");
-        out.println("            height: 100px;");
-        out.println("        }");
-        out.println("    </style>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("    <table>");
-        out.println("        <tr>");
-        out.println("            <th>id</th>");
-        out.println("            <th>logo</th>");
-        out.println("            <th>name</th>");
-        out.println("        </tr>");
-        for (Restaurant res: dataset.getRestaurants()){
-            out.println("<tr>");
-            out.println("<td>"+res.getId()+"</td>");
-            out.println("<td><img class=\"logo\" src=\""+res.getLogo()+"\" alt=\"logo\"></td>");
-            out.println("<td>"+res.getName()+"</td>");
-            out.println("</tr>");
-        }
-        out.println("    </table>");
-        out.println("</body>");
-        out.println("</html>");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("index.jsp");
+
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,43 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Restaurants</title>
+    <style>
+        table {
+            text-align: center;
+            margin: auto;
+        }
+        th, td {
+            padding: 5px;
+            text-align: center;
+        }
+        .logo{
+            width: 100px;
+            height: 100px;
+        }
+    </style>
+</head>
 <body>
-<h2>Hello World!</h2>
+<table>
+    <tr>
+        <th>id</th>
+        <th>logo</th>
+        <th>name</th>
+        <th>location</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td><img class="logo" src="https://picsum.photos/536/354" alt="logo"></td>
+        <td>restaurant 1 name</td>
+        <td>(10, 7)</td>
+    </tr>
+    <tr>
+        <td>2</td>
+        <td><img class="logo" src="https://picsum.photos/536/354" alt="logo"></td>
+        <td>restaurant 2 name</td>
+        <td>(210, 99)</td>
+    </tr>
+</table>
 </body>
 </html>

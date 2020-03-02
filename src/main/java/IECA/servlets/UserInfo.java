12f8d030b02a9794a754935ae9d2bc17c6abd4d9
leilafakheri <fakheri90@gmail.com>
2020-03-02 18:10:37 +0330

    UserInfo.java cleaned up

--- src/main/java/IECA/servlets/UserInfo.java
+++ src/main/java/IECA/servlets/UserInfo.java
@@ -12,14 +12,23 @@ import java.io.IOException;
 
 @WebServlet("/UserInfo")
 public class UserInfo extends HttpServlet {
+    private ServletHandler servletHandler;
+
+    public void initial(){
+        servletHandler = new ServletHandler();
+    }
+
+    public void dispatch(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        servletHandler.dispatchTo(request,response,"userInfo.jsp");
+    }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String value = request.getParameter("credit");
+        initial();
         if(!(value.equals(""))){
             RestaurantManager.getInstance().getCurrentUser().addCredit(Integer.valueOf(value));
         }
-        String userInfo = "userInfo.jsp";
-        RequestDispatcher requestDispatcher = request.getRequestDispatcher(userInfo);
-        requestDispatcher.forward(request, response);
+        dispatch(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {

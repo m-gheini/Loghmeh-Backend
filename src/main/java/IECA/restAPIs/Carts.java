122300a8e1e471b68e8a119764b611387b23c029
m-gheini <mahya.gheini78@gmail.com>
2020-04-07 13:10:53 +0430

    cart json page updated.

--- src/main/java/IECA/restAPIs/Carts.java
+++ src/main/java/IECA/restAPIs/Carts.java
@@ -23,6 +23,44 @@ public class Carts {
         else
             return userCart;
     }
+    @RequestMapping(value = "/users", method = RequestMethod.POST)
+    public @ResponseBody
+    Object addUser(
+            @RequestParam(value = "restaurantId") String restaurantId,
+            @RequestParam(value = "foodName") String foodName) throws IOException {
+        Cart userCart = RestaurantManager.getInstance().getCurrentUser().getMyCart();
+        ArrayList<Food> foods = new ArrayList<Food>();
+        ArrayList<Cart> orders = new ArrayList<Cart>();
+        if(userCart.getFoods().size()==0 && userCart.getSaleFoods().size()==0){
+            Cart myCart = new Cart();
+            return myCart;
+        }
+        else{
+            if(userCart.getSaleFoods().size()!=0) {
+                if (restaurantId.equals(userCart.getSaleFoods().get(0).getRestaurantId())){
+                    return userCart;
+                }
+                else{
+                    Error error = new Error();
+                    error.setErrorCode(400);
+                    error.setErrorMassage("You can not order from different restaurant!");
+                    return error;
+                }
+            }
+            if(userCart.getFoods().size()!=0) {
+                if (restaurantId.equals(userCart.getFoods().get(0).getRestaurantId())){
+                    return userCart;
+                }
+                else{
+                    Error error = new Error();
+                    error.setErrorCode(400);
+                    error.setErrorMassage("You can not order from different restaurant!");
+                    return error;
+                }
+            }
+        }
+        return userCart;
+    }
 
 
 

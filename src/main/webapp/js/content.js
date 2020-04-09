7803d0734a101cffd52052662adc20b6a2bec472
m-gheini <mahya.gheini78@gmail.com>
2020-04-09 16:34:52 +0430

    footer by react added.

--- src/main/webapp/js/content.js
+++ src/main/webapp/js/content.js
@@ -1,6 +1,6 @@
 function FooterSection() {
     return(
-        <footer class="footer" dir="rtl"  lang="fa">
+        <footer className="footer" dir="rtl"  lang="fa">
             <p lang="fa">&copy; تمامی حقوق متعلق به لقمه است.</p>
         </footer>
     );

8c02bddc903d0b3f6ef97e719382a6f020ea3180
leilafakheri <fakheri90@gmail.com>
2020-04-09 12:02:21 +0430

    saleFood get api added

--- src/main/java/IECA/restAPIs/Restaurants.java
+++ src/main/java/IECA/restAPIs/Restaurants.java
@@ -22,6 +22,45 @@ public class Restaurants {
         ArrayList<SaleFood> saleFoods = RestaurantManager.getInstance().getSaleFoods();
         return saleFoods;
     }
+    @RequestMapping(value = "/saleFoods/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    ArrayList<SaleFood> allSaleFoodsOfRestaurant(@PathVariable(value = "id") String id) throws IOException {
+        ArrayList<SaleFood> saleFoods = RestaurantManager.getInstance().getSaleFoods();
+        ArrayList<SaleFood> result = new ArrayList<SaleFood>();
+        for(SaleFood sf:saleFoods){
+            if(sf.getRestaurantId().equals(id))
+                result.add(sf);
+        }
+        return result;
+    }
+    @RequestMapping(value = "/saleFoods/{id}",params = "saleFoodName",method = RequestMethod.GET)
+    public @ResponseBody
+    Object specificSaleFood(@PathVariable(value = "id") String id,
+                              @RequestParam(value = "saleFoodName") String saleFoodName) throws IOException {
+        ArrayList<SaleFood> saleFoods = RestaurantManager.getInstance().getSaleFoods();
+        boolean restaurantFound = false;
+        boolean foodFound = false;
+        SaleFood result = new SaleFood();
+        for(SaleFood sf:saleFoods){
+            if(sf.getRestaurantId().equals(id)) {
+                restaurantFound = true;
+                if(sf.getName().equals(saleFoodName)) {
+                    foodFound = true;
+                    result = sf;
+                    return result;
+                }
+            }
+        }
+        Error error = new Error();
+        if (!restaurantFound) {
+            error.setErrorMassage("no such restaurant");
+            error.setErrorCode(404);
+        }
+        error.setErrorCode(404);
+        error.setErrorMassage("no such saleFood");
+        return error;
+    }
+
     @RequestMapping(value = "/restaurants/{id}" , method = RequestMethod.GET)
     public @ResponseBody
     Object specificRestaurant(@PathVariable(value = "id") String id) throws IOException {

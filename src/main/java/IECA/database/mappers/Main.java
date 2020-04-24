81241380561084950c5cc5fb4208586103188964
m-gheini <mahya.gheini78@gmail.com>
2020-04-24 17:46:17 +0430

    FoodPartyMapper test added.

--- src/main/java/IECA/database/mappers/Main.java
+++ src/main/java/IECA/database/mappers/Main.java
@@ -2,6 +2,7 @@ package IECA.database.mappers;
 
 import IECA.logic.Food;
 import IECA.logic.Location;
+import IECA.logic.SaleFood;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -9,23 +10,24 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        FoodMapper fm = new FoodMapper(false);
+        IFoodPartyMapper fpm = new FoodPartyMapper(true);
         Connection connection = ConnectionPool.getConnection();
-        Food f1 = new Food();
+        SaleFood f1 = new SaleFood();
         f1.setName("پیتزل مخصوص");
         f1.setDescription("تهیه شده از بهترین مواد اولیه");
         f1.setPopularity((float) 0.9);
+        f1.setCount(3);
+        f1.setOldPrice(20000);
         f1.setPrice(17000);
-        f1.setRestaurantId("5e4fcf14af68ed25d5900ebb");
+        f1.setRestaurantId("5e4fcf14af68ed25d5900ebc");
         f1.setImage("https://static.snapp-food.com/200x201/cdn/29/26/9/vendor/5d346019a73f7.jpg");
-        fm.insert(f1);
+        fpm.insert(f1);
 
         ArrayList<String> keys = new ArrayList<String>();
         keys.add("پیتزل مخصوص");
-        keys.add("5e4fcf14af68ed25d5900ebb");
-        Food f = fm.find(keys);
+        keys.add("5e4fcf14af68ed25d5900ebc");
+        Food f = fpm.find(keys);
         System.out.println(f);
-        fm.delete(keys);
 //        Location l = lm.find(1);
 //        System.out.println(l);
 //        lm.delete(2);

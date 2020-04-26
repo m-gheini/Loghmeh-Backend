898e1553e1c53a14571890500657668f0a45624a
leila <fakheri90@gmail.com>
2020-04-26 16:20:22 +0430

    saleOrder table added

---
+++ src/main/java/IECA/database/mappers/ISaleOrderMapper.java
@@ -0,0 +1,6 @@
+package IECA.database.mappers;
+
+import IECA.logic.Cart;
+
+public interface ISaleOrderMapper extends IMapper<Cart, Integer> {
+}
---
+++ src/main/java/IECA/database/mappers/SaleOrderMapper.java
@@ -0,0 +1,105 @@
+package IECA.database.mappers;
+
+import IECA.logic.Cart;
+import IECA.logic.SaleFood;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class SaleOrderMapper extends Mapper<Cart, Integer> implements ISaleCartMapper {
+    private static final String COLUMNS = " id, number, index, status, restaurantId, name, restaurantName";
+    private static final String TABLE_NAME = "saleOrder_table";
+
+    private Boolean doManage;
+
+    public SaleOrderMapper(Boolean doManage) throws SQLException {
+        if (this.doManage = doManage) {
+            Connection con = ConnectionPool.getConnection();
+            Statement st = con.createStatement();
+            st.executeUpdate(String.format("DROP TABLE IF EXISTS %s", TABLE_NAME));
+            st.executeUpdate(String.format(
+                    "CREATE TABLE  %s " +
+                            "(" +
+                            "id int, " +
+                            "number int, " +
+                            "index int, " +
+                            "status varchar(255), " +
+                            "restaurantId varchar(255), " +
+                            "name varchar(255), " +
+                            "restaurantName varchar(255), " +
+                            "PRIMARY KEY(id, name, index), " +
+                            "foreign key(restaurantId, name) references foodParty_table(restaurantId, name) on delete  cascade, " +
+                            "foreign key(id) references user_table(id) on delete  cascade" +
+                            ");",
+                    TABLE_NAME));
+            st.close();
+            con.close();
+        }
+    }
+
+    @Override
+    protected String getFindStatement(ArrayList<Integer> keys) {
+        if(keys.size()==2){
+            Integer id = keys.get(0);
+            Integer index = keys.get(1);
+            return "SELECT " + COLUMNS +
+                    " FROM " + TABLE_NAME +
+                    " WHERE id = " + id + "and index = " + index + ";";
+
+        }
+        Integer id = keys.get(0);
+        return "SELECT " + COLUMNS +
+                " FROM " + TABLE_NAME +
+                " WHERE id = " + id + ";";
+    }
+
+    @Override
+    protected String getInsertStatement(Cart cart) {
+        return "INSERT IGNORE INTO " + TABLE_NAME +
+                "(" + COLUMNS + ")" + " VALUES "+
+                "("+
+                cart.getUserId()+ "," +
+                cart.getNumberOfSaleFood().get(0) + "," +
+                cart.getIndex()+ "," +
+                "'" + cart.getStatus() + "'," +
+                "'" + cart.getSaleFoods().get(0).getRestaurantId() + "'," +
+                "'" + cart.getSaleFoods().get(0).getName() + "'" +
+                "'" + cart.getSaleFoods().get(0).getRestaurantName() + "'" +
+                ") ON DUPLICATE KEY UPDATE number = " + cart.getStatus() +";";
+    }
+    @Override
+    protected String getDeleteStatement(ArrayList<Integer> keys) {
+        Integer id = keys.get(0);
+        return "DELETE FROM " + TABLE_NAME +
+                " WHERE id = " + id + ";";
+    }
+
+    @Override
+    protected Cart convertResultSetToObject(ResultSet rs) throws SQLException {
+        Cart cart = new Cart();
+        cart.setUserId(rs.getInt(1));
+        cart.setIndex(rs.getInt(3));
+        cart.setStatus(rs.getString(4));
+        FoodPartyMapper foodPartyMapper = new FoodPartyMapper(false);
+//        RestaurantMapper restaurantMapper = new RestaurantMapper(false);
+        Connection connection = ConnectionPool.getConnection();
+//        ArrayList<String > resId = new ArrayList<String>();
+//        resId.add(rs.getString(3));
+        cart.setRestaurantName(rs.getString(7));
+        ArrayList<String> keys = new ArrayList<String>();
+        keys.add(rs.getString(6));
+        keys.add(rs.getString(5));
+        ArrayList<SaleFood> foodParties = new ArrayList<SaleFood>();
+        ArrayList<Integer> numberOfSaleFoods = new ArrayList<Integer>();
+        foodParties.add(foodPartyMapper.find(keys));
+        numberOfSaleFoods.add(rs.getInt(2));
+        cart.setSaleFoods(foodParties);
+        cart.setNumberOfSaleFood(numberOfSaleFoods);
+        connection.close();
+        return  cart;
+    }
+
+}
